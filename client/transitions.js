93018823428f57dc92b26c1e243cbae83156e174
g-k-l <kevin.liu@simplelegal.com>
2018-07-13 16:18:29 -0700

    refactor, cleanup transitionLinks

--- client/transitions.js
+++ client/transitions.js
@@ -10,15 +10,23 @@ var TRANSITIONS = (function() {
     DURATION = 1500;
 
   function transitionLinks(selection) {
+
+    /* base on the bounded-data, calculates
+      the property "d" which is the instruction 
+      on how to draw the links */
+    var link_generator = d3
+      .linkRadial()
+      .angle(function(d) {
+        return d.x;
+      })
+      .radius(function(d) {
+        return d.y;
+      });
+
+    /* assign transition to the selected links */
     selection
       .attr("d", function(d) {
-        return d3.linkRadial()
-          .angle(function(d) {
-            return d.x;
-          })
-          .radius(function(d) {
-            return d.y;
-          })({
+        return link_generator({
           source: d.source,
           target: d.source
         });
@@ -28,21 +36,12 @@ var TRANSITIONS = (function() {
         return DELAY * d.target.depth;
       })
       .duration(DURATION)
-      .attr(
-        "d",
-        d3.linkRadial()
-          .angle(function(d) {
-            return d.x;
-          })
-          .radius(function(d) {
-            return d.y;
-          })
-      );
+      .attr("d", link_generator);
     return selection;
   }
 
   module_export.DELAY = DELAY;
   module_export.DURATION = DURATION;
   module_export.transitionLinks = transitionLinks;
-  return module_export
+  return module_export;
 })();

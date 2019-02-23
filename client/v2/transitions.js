e748615f855a8078dbb31a16f57ae6c4c93df0b2
Kevin Liu <gkliu93@msn.com>
2019-02-22 18:22:52 -0800

    clean up code for initial page load transitions

--- client/v2/transitions.js
+++ client/v2/transitions.js
@@ -1,6 +1,6 @@
 /*
-  Contains logic related to transitioning SVGs
-  from one CSS class to another.
+  Contains logic which handle the initial
+  transition on page load.
 */
 
 var TRANSITIONS = (function() {
@@ -33,8 +33,6 @@ var TRANSITIONS = (function() {
       };
     } else if (start_or_end === "end") {
       return link_generator;
-    } else {
-      throw "Must be either start or end";
     }
   }
 
@@ -42,38 +40,18 @@ var TRANSITIONS = (function() {
     return DELAY * (d.target.depth - 1) + i*DELAY_MULT;
   }
 
-  function transitionLinks(selection, start_state, end_state) {
+  function transitionLinksEnter(selection) {
     /* assign transition to the selected links */
     let transitions = selection
-      .attr("d", start_state)
+      .attr("d", linkState("start"))
       .transition("links")
       .delay(function(d, i) {
         return DELAY * (d.target.depth - 1) + i*DELAY_MULT;
       })
       .duration(DURATION)
-      .attr("d", end_state);
+      .attr("d", linkState("end"));
     return transitions;
   }
-
-  function transitionLinksEnter(selection) {
-    return transitionLinks(selection, linkState("start"), linkState("end"));
-  }
-
-  function transitionLinksExit(selection) {
-    return transitionLinks(selection, linkState("end"), linkState("start"));
-  }
-
-  function updateLinks(selection) {
-    return selection
-      .attr("d", linkState("start"))
-      .transition("update-links")
-      .delay(function(d, i) {
-        return DELAY * (d.target.depth - 1) + i*DELAY_MULT;
-      })
-      .duration(DURATION)
-      .attr("d", linkState("end"));
-  }
-
 
   function nodeState(start_or_end) {
     let final_location = function(d) {
@@ -91,66 +69,19 @@ var TRANSITIONS = (function() {
       return initial_location
     } else if (start_or_end === "end") {
       return final_location
-    } else {
-      throw "Must be either start or end"
     }
   }
 
-  function transitionNodes(selection, start_state, end_state) {
+  function transitionNodesEnter(selection) {
     let transitions = selection
-      .attr("transform", start_state)
+      .attr("transform", nodeState("start"))
       .transition("nodes")
       .delay(function(d, i) {
         return DELAY * (d.depth - 1) + i*DELAY_MULT;
       })
       .duration(DURATION)
-      .attr("transform", end_state);
+      .attr("transform", nodeState("end"));
     return transitions;
-  }
-
-  function transitionNodesEnter(selection) {
-    return transitionNodes(selection, nodeState("start"), nodeState("end"))
-  }
-
-  function transitionNodesExit(selection) {
-    return transitionNodes(selection, nodeState("end"), nodeState("start"))
-  }
-
-  function updateNodes(selection) {
-
-    function trans(d) {
-      console.log(d);
-      var end  = projection(d.x, d.y);
-      var start;
-      if (d.parent === null) {
-        start = [0, 0];
-      } else {
-        start = projection(d.x0, d.y0);
-      }
-      var trans = [end[0]-start[0], end[1]-start[1]];
-      // console.log(trans);
-      return "translate(" + trans + ")" 
-      // if (d.parent === null) {
-      //   return "translate(" + projection(d.x, d.y) + ")";
-      // } else {
-      //   return "translate(" + projection(d.x-d.parent.x, d.y-d.parent.y) + ")";
-      // }
-      
-    }
-    return selection
-      .transition("update-nodes")
-      .delay(function(d, i) {
-        return DELAY * (d.depth - 1) + i*DELAY_MULT;
-      })
-      .duration(DURATION)
-      // .attr("transform", nodeState("end"));
-      .attr("transform", trans); 
-      // .attr("cx", function(d){
-      //   return projection(d.x, d.y)[0];
-      // })
-      // .attr("cy", function(d){
-      //   return projection(d.x, d.y)[1];
-      // })
   }
 
   function circleFadeIn(selection) {
@@ -163,8 +94,6 @@ var TRANSITIONS = (function() {
       .duration(DURATION)
       .attr("r", CIRCLE_SIZE);
   }
-
-  function circleFadeOut(selection) {}
 
   function nameFadeIn(selection) {
     return selection
@@ -179,10 +108,7 @@ var TRANSITIONS = (function() {
           .duration(DURATION)
           .style("opacity", 1);  
       })
-      
   }
-
-  function nameFadeOut(selection) {}
 
   function rotate_text(d) {
     if (d.x < Math.PI) {
@@ -192,15 +118,11 @@ var TRANSITIONS = (function() {
     }
   }
 
-  module_export.updateLinks = updateLinks;
-  module_export.updateNodes = updateNodes;
   module_export.DELAY = DELAY;
   module_export.DELAY_MULT = DELAY_MULT;
   module_export.DURATION = DURATION;
   module_export.transitionLinksEnter = transitionLinksEnter;
-  module_export.transitionLinksExit = transitionLinksExit;
   module_export.transitionNodesEnter = transitionNodesEnter;
-  module_export.transitionNodesExit = transitionNodesExit;
   module_export.circleFadeIn = circleFadeIn;
   module_export.nameFadeIn = nameFadeIn;
   return module_export;

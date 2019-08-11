a66c5c3c299b6b0dca74eae31fd2879c398e2899
Kevin Liu <gkliu93@msn.com>
2019-08-11 01:26:01 -0700

    misc clean up, use https in production

--- config/config.js
+++ config/config.js
@@ -1,9 +1,8 @@
 var dotenv = require("dotenv").config()
-var port = process.env.PORT
-if (process.env.ENVIRONMENT != "PRODUCTION") {
-	port = 8080
-} 
+var port = 8080
+
 module.exports = {
-	port: port,
-	mongo_uri: process.env.MONGO_URI
+    ENV: process.env.ENVIRONMENT,
+    port: port,
+    mongo_uri: process.env.MONGO_URI
 }
--- server.js
+++ server.js
@@ -5,48 +5,53 @@ var mongo = require("./lib/mongo");
 var utils = require("./lib/utils");
 var collection_utils = require("./lib/collection_utils");
 var config = require("./config/config");
+var https = require("https")
+var http = require("http")
 
 app.set("trust proxy", true);
-app.use(utils.wwwRedirect);
 app.use(express.static(__dirname));
 app.use(express.static(__dirname + "/client"));
 
 app.route("/").get(function(req, res) {
-	res.redirect("/v2")
+  res.redirect("/v2")
 });
 
 app.route("/v1").get(function(req, res) {
-	res.sendFile(__dirname + "/client/v1/tree.html");
+  res.sendFile(__dirname + "/client/v1/tree.html");
 })
 
 app.route("/v2").get(function(req, res) {
-	res.sendFile(__dirname + "/client/v2/tree.html");
+  res.sendFile(__dirname + "/client/v2/tree.html");
 })
 
 app.route("/tree/:math_ids").get(function(req, res) {
-	try {
-		var id_array = req.params.math_ids.split(",").map(Number);
-		collection_utils.fetchById(id_array, function(items) {
-			res.json(items);
-		});
-	} catch (e) {
-		res.json({ msg: "parseInt failed. Error: " + e });
-	}
+  try {
+    var id_array = req.params.math_ids.split(",").map(Number);
+    collection_utils.fetchById(id_array, function(items) {
+      res.json(items);
+    });
+  } catch (e) {
+    res.json({ msg: "parseInt failed. Error: " + e });
+  }
 });
 
 app.route("/tree/name/:root_name").get(function(req, res) {
-	collection_utils.fetchByName(req.params.root_name, function(items) {
-		res.json(items);
-	});
+  collection_utils.fetchByName(req.params.root_name, function(items) {
+    res.json(items);
+  });
 });
 
 // start app here
-var collection;
 mongo.connect(function(database) {
-	mongodb = database;
-	collection = mongodb.collection("phds2");
-	collection_utils = collection_utils.init(collection);
-	app.listen(config.port, function() {
-		console.log("Listening at Port " + config.port);
-	});
+  var mongodb = database;
+  var collection = mongodb.collection("phds2");
+  collection_utils = collection_utils.init(collection);
+
+  if (config.ENV === "PRODUCTION") {
+    https.createServer({}, app).listen(443);
+  } else {
+    http.createServer(app, function() {
+      console.log("Listening at Port 80");
+    }).listen(80)
+  }
 });

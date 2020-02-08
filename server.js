2dc5df2e8b4111f6447694363762705316f6ba85
Kevin Liu <gkliu93@msn.com>
2020-02-08 13:02:33 -0800

    remove static file endpoints - replaced with s3proxy

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const config = require("./config/config");
 var collection_utils = require("./lib/collection_utils");
 
 
-const s3ProxyWrapper = function(req, res, next) {
+const s3ProxyWrapperTicTacToe = function(req, res, next) {
   /*modify the URL so that TICTACTOE_URL_PATH is not prepended
     as a directory/prefix in the S3 bucket path */
   req.originalUrl = req.originalUrl.replace(config.TICTACTOE_URL_PATH, '')
@@ -26,6 +26,20 @@ const s3ProxyWrapper = function(req, res, next) {
     defaultKey: 'index.html',
   })(req, res, next);
 };
+
+const s3ProxyWrapperGenealogy = function(req, res, next) {
+  /* Route genealogy static urls to the corresponding s3 bucket */ 
+  req.originalUrl = req.originalUrl.replace(config.GENEALOGY_URL_PATH, '')
+  return s3Proxy({
+    bucket: config.GENEALOGY_BUCKET_NAME,
+    prefix: '',
+    accessKeyId: config.AWS_ACCESS_KEY_ID,
+    secretAccessKey: config.AWS_SECRET_ACCESS_KEY,
+    overrideCacheControl: 'max-age=2592000',
+    defaultKey: 'v2/tree.html',
+  })(req, res, next);
+};
+
 
 /*Heroku, nodejitsu and other hosters often use reverse proxies
   which offer SSL endpoints but then forward unencrypted HTTP
@@ -44,16 +58,8 @@ app.use(express.static(__dirname));
 app.use(express.static(__dirname + "/client"));
 
 app.route("/").get(function(req, res) {
-  res.redirect("/v2")
+  res.redirect("/genealogy")
 });
-
-app.route("/v1").get(function(req, res) {
-  res.sendFile(__dirname + "/client/v1/tree.html");
-})
-
-app.route("/v2").get(function(req, res) {
-  res.sendFile(__dirname + "/client/v2/tree.html");
-})
 
 app.route("/tree/:math_ids").get(function(req, res) {
   try {
@@ -73,8 +79,10 @@ app.route("/tree/name/:root_name").get(function(req, res) {
 });
 
 /* endpoints for serving tic-tac-toe React app */
-app.get(config.TICTACTOE_URL_PATH, s3ProxyWrapper);
-app.get(config.TICTACTOE_URL_PATH + "/*", s3ProxyWrapper);
+app.get(config.TICTACTOE_URL_PATH, s3ProxyWrapperTicTacToe);
+app.get(config.TICTACTOE_URL_PATH + "/*", s3ProxyWrapperTicTacToe);
+app.get(config.GENEALOGY_URL_PATH, s3ProxyWrapperGenealogy);
+app.get(config.GENEALOGY_URL_PATH + "/*", s3ProxyWrapperGenealogy);
 
 function logListen(port) {
   console.log("Listening on port: " + port);

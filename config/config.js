5dec9a2befec020d6463767af1132a8c7bd0b060
Kevin Liu <gkliu93@msn.com>
2020-02-08 11:58:29 -0800

    add s3Proxy for genealogy static

--- config/config.js
+++ config/config.js
@@ -8,6 +8,8 @@ module.exports = {
     mongo_uri: process.env.MONGO_URI,
     TICTACTOE_URL_PATH: process.env.TICTACTOE_URL_PATH,
     TICTACTOE_BUCKET_NAME: process.env.TICTACTOE_BUCKET_NAME,
+    GENEALOGY_URL_PATH: process.env.GENEALOGY_URL_PATH,
+    GENEALOGY_BUCKET_NAME: process.env.GENEALOGY_BUCKET_NAME,
     AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
     AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
 }
